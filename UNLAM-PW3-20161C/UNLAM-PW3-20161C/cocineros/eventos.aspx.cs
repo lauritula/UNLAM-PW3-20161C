581f12f1a0da976d9c8e011d738ffaf15747959c
lauritula <mlaura.tula@gmail.com>
2016-04-27 16:47:30 -0300

    Estructura inicial del proyecto

---
+++ UNLAM-PW3-20161C/UNLAM-PW3-20161C/cocineros/eventos.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UNLAM_PW3_20161C.cocineros
+{
+    public partial class eventos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ UNLAM-PW3-20161C/UNLAM-PW3-20161C/comensales/reservar.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UNLAM_PW3_20161C.comensales
+{
+    public partial class reservar : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
